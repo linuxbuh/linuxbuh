0a8a51a74e7926e4c499b0848ff6527d15dcd0a2
linuzbuh <linuxbuh@yandex.ru>
2021-09-25 07:28:16 +0300

    add sbis3plugin

---
+++ app-crypt/sbis-libstdc/sbis-libstdc-0.1.ebuild
@@ -0,0 +1,36 @@
+# Copyright 1999-2018 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Id$
+
+EAPI=7
+inherit font gnome2-utils eutils multilib unpacker
+
+DESCRIPTION="Библиотека СБИС stdc++"
+HOMEPAGE="https://sbis.ru/"
+
+KEYWORDS="amd64"
+
+SRC_URI="amd64? ( https://update-msk1.sbis.ru/Sbis3Plugin/rc/linux/deb_repo/sbis-libstdc++.deb -> sbis-libstdc-0.1.deb )"
+
+SLOT="0"
+RESTRICT="strip mirror"
+LICENSE="GPL-3"
+IUSE=""
+
+
+NATIVE_DEPEND="sys-libs/glibc
+	sys-devel/gcc
+ "
+
+RDEPEND="
+    ${NATIVE_DEPEND}
+"
+DEPEND="${RDEPEND}"
+
+S="${WORKDIR}"
+
+src_install() {
+
+	cp -R "${WORKDIR}/opt" "${D}" || die "install failed!"
+	cp -R "${WORKDIR}/usr" "${D}" || die "install failed!"
+}
---
+++ app-crypt/sbis3plugin/files/sbis3plugin-postinst.sh
@@ -0,0 +1,323 @@
+#!/bin/bash
+
+# create install temporary log file
+logname="sbis3plugin-postinstall"
+
+# load logging functions
+source "/usr/bin/sbis3plugin-install.common.sh"
+echo_with_log "-------------"
+echo_with_log "Postinstall script"
+echo_with_log "Installer Value: $1"
+
+# move temp directory to original
+command_call "mv /opt/sbis3plugin/temp/* $sbis3plugin_path/ && rm -rf /opt/sbis3plugin/temp/"
+
+# install cert
+variable_value cert_path
+echo_with_log "Установка сертификата для всех пользователей"
+
+if [ -d /usr/share/pki/ca-trust-source/ ]; then
+   command_call "cp \"$cert_path\" /usr/share/pki/ca-trust-source/anchors/sbis.pem"
+elif [ -d /usr/share/pki/trust/ ]; then
+   command_call "cp \"$cert_path\" /usr/share/pki/trust/anchors/sbis.pem"
+else
+   command_call "cp \"$cert_path_pem\" /usr/local/share/ca-certificates/sbis.crt"
+fi
+   
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось копировать сертификат. Установка не завершена."
+    exit 1
+fi
+
+if command -v update-ca-trust; then
+   command_call "update-ca-trust"
+elif command -v update-ca-certificates; then
+   command_call "update-ca-certificates"
+   command_call "c_rehash"
+else
+   echo_with_log "Не удалось установить сертификат. Нет команды обновления сертификатов."
+   exit 1
+fi
+
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось установить сертификат. Установка не завершена."
+    exit 1
+fi
+
+if [ ! -e /etc/pki/tls/certs/ca-bundle.crt ] && [ -e /etc/ssl/certs/ca-certificates.crt ]; then
+   mkdir -p /etc/pki/tls/certs/
+   ln -sf /etc/ssl/certs/ca-certificates.crt /etc/pki/tls/certs/ca-bundle.crt 
+fi
+
+# remove all except *.ini files and "logs" folder
+command_call "ls -d /usr/share/Sbis3Plugin/* | grep -E -v \".ini$|/logs$\" | xargs rm -rfv"
+
+# create application icon
+variable_value app_info_name
+variable_value app_info_dir
+variable_value app_info_path
+
+command_call "rm -f \"$app_info_path\""
+cat > "$app_info_path" << EOF
+#!/usr/bin/env xdg-open
+
+[Desktop Entry]
+Name=СБИС3 Плагин
+Comment=Tensor
+Exec=$sbis3plugin_path/sbis3plugin %U
+Path=$sbis3plugin_path
+Icon=$sbis3plugin_path/icons/default_00.png
+Terminal=false
+Type=Application
+Encoding=UTF-8
+Categories=Network;
+StartupNotify=true
+EOF
+command_call "chmod +x \"$app_info_path\""
+
+# register autorun
+echo_with_log "Регистрация автозапуска"
+variable_value autorun_dir
+variable_value autorun_path
+command_call "mkdir -p \"$autorun_dir\""
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось создать директорию автозапуска. Установка не завершена."
+    exit 1
+fi
+command_call "rm -f \"$autorun_path\""
+cat > "$autorun_path" << EOF
+#!/usr/bin/env xdg-open
+
+[Desktop Entry]
+Name=СБИС3 Плагин
+Comment=Tensor
+Exec=$sbis3plugin_path/sbis3plugin %U --autostart
+Path=$sbis3plugin_path
+Icon=$sbis3plugin_path/icons/default_00.png
+Terminal=false
+Type=Application
+Encoding=UTF-8
+Categories=Network;
+StartupNotify=true
+EOF
+command_call "chmod +x \"$autorun_path\""
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось создать файл автозапуска. Установка не завершена."
+    exit 1
+fi
+
+# register nmh
+echo_with_log "Регистрация Chrome NMH и SbisPluginConnector, проверка признака RC версии"
+current_locale=$(echo $LANG | cut -d "." -f1 | tr _ -)
+variable_value current_locale
+if [ ! -z $current_locale ]; then
+   current_locale="locale=$current_locale"
+fi
+command_call "\"$sbis3plugin_path/21.4204.33/service/components-registrator\" installActions $current_locale"
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось выполнить регистрацию дополнительных компонентов. Установка не завершена."
+    exit 1
+fi
+
+# kill ChromeNmhTransport for change version
+echo_with_log "Обновление версии CromeNmhTransport"
+command_call "killall -9 ChromeNmhTransport"
+
+# mark files as verified
+command_call "touch \"$sbis3plugin_path/21.4204.33/integrity.checked\""
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось создать файл признака корректности файлов. Установка не завершена."
+    exit 1
+fi
+
+# Create marker file for afterInstall action
+command_call "mkdir \"/usr/share/Sbis3Plugin\""
+command_call "touch \"/usr/share/Sbis3Plugin/afterInstall.marker\""
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось создать маркерный файл для выполнения afterInstall. Установка не завершена."
+    exit 1
+fi
+
+# register daemon
+echo_with_log "Регистрация демона"
+command_call "bash \"$sbis3plugin_path/21.4204.33/service/sbis-daemon-setup.sh\" --daemon-name SBIS3Plugin uninstall"
+
+if [ -z "$USER" ] && [ -z "$SUDO_USER" ]; then
+   echo_with_log "НЕОБХОДИМО ПЕРЕЗАГРУЗИТЬ КОМПЬЮТЕР ПОСЛЕ УСТАНОВКИ"
+fi
+
+command_call "bash \"$sbis3plugin_path/21.4204.33/service/sbis-daemon-setup.sh\" --force --library \"auto\" --ep \"auto\" --autorun --directory \"$sbis3plugin_path\" --executable-name sbis3plugin --add-opts \"--daemon --output_file \"/usr/share/Sbis3Plugin/logs/service_daemon.log\"\" --daemon-name SBIS3Plugin --user root install"
+
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось зарегистрировать демона. Установка не завершена."
+    exit 1
+fi
+command_call "bash \"$sbis3plugin_path/21.4204.33/service/update_scripts/addDaemonRestart.sh\""
+command_call "service SBIS3Plugin start"
+if [ $? -ne 0 ]
+  then
+    echo_with_log "Не удалось запустить демона. Установка не завершена."
+    exit 1
+fi
+
+if { [ "$os_name" == "centos" ] || [ "$os_name" == "red soft" ]; } && [ -e /etc/abrt.conf ]; then
+   echo_with_log "Настройка abrtd."
+   sed -i 's,^\(MaxCrashReportsSize = \).*$,\15000,' /etc/abrt/abrt.conf >/dev/null 2>&1
+   sed -i 's,^\(OpenGPGCheck = \).*$,\1no,' /etc/abrt/abrt-action-save-package-data.conf >/dev/null 2>&1
+   sed -i 's,^\(ProcessUnpackaged = \).*$,\1yes,' /etc/abrt/abrt-action-save-package-data.conf >/dev/null 2>&1
+   systemctl restart abrtd >/dev/null 2>&1
+   for f in $SYSTEMD_ROOT/multi-user.target.wants/abrt-*.service; do
+     SRV=$(basename "$f")
+     systemctl restart "$SRV"
+   done 
+fi
+
+# Wait for afterInstall result
+i="0"
+seconds_to_wait="60"
+while [ $i -le $seconds_to_wait ]
+do
+    i=$[ $i + 1 ]
+
+    #success
+    if [ -f "/usr/share/Sbis3Plugin/afterInstall.success" ]
+    then
+        echo_with_log "Выполнение afterInstall успешно завершено"
+        break
+    fi
+
+    #fail
+    if [ -f "/usr/share/Sbis3Plugin/afterInstall.fail" ]
+    then
+        echo_with_log "Выполнение afterInstall завершилось с ошибкой. Установка не завершена."
+        exit 1  
+    fi
+    
+    #timeout
+    if [ $i -gt $seconds_to_wait ]
+    then
+        echo_with_log "Не удалось выполнить afterInstall за отведенное время. Установка не завершена."
+        exit 1
+    fi
+
+    sleep 1
+done
+
+# create per-user installer
+variable_value per_user_dir
+variable_value per_user_install
+variable_value per_user_uninstall
+command_call "rm -f \"$per_user_uninstall\""
+
+cat > "$per_user_install" << EOF
+#!/bin/bash
+
+# create install temporary log file
+logname="sbis3plugin-per-user-install_\$USER"
+
+# create per user install temporary log file
+per_user_install_done=".Sbis3Plugin/install.done.21.4204.33"
+eval HOME_DIR=~"\$USER"
+mark_file="\$HOME_DIR/\$per_user_install_done"
+
+if [ ! -e "\$mark_file" ]; then
+   # load logging functions
+   source "/usr/bin/sbis3plugin-install.common.sh"
+   variable_value per_user_install_done
+   variable_value per_user_uninstall_done
+   variable_value cert_path
+   variable_value mark_file
+   mark_file_dir=\$(dirname "\$mark_file")
+   variable_value mark_file_dir
+   command_call "mkdir -p \"\$mark_file_dir\""
+   command_call "rm -f \"\$HOME_DIR/.Sbis3Plugin/uninstall.done.\"*"
+   command_call "rm -f \"\$HOME_DIR/.Sbis3Plugin/install.done.\"*"
+   command_call "touch \"\$mark_file\""
+   if [ \$EUID -ne 0 ]; then
+      # Если хранилище сертификатов Debian-based, то установки сертифика в него
+      # не хватит для работы браузеров с плагином через web-socket, ибо в Debian это хранилище игнорируется браузерами
+      if ! command -v update-ca-trust; then
+         # install ff cert
+         echo_with_log "Регистрация сертификата для Firefox"
+         # first output result fo command: find "\$HOME_DIR/.mozilla/firefox" -name "cert8.db" | while read -r certDB
+         command_call "find \"\$HOME_DIR/.mozilla/firefox\" -name \"cert8.db\""
+         # then use this result in cycle
+         cert_name="TensorCA"
+         variable_value cert_name
+         find "\$HOME_DIR/.mozilla/firefox" -name "cert8.db" | while read -r certDB
+         do
+            certdir=\$(dirname "\$certDB")
+            variable_value certdir
+            command_call "certutil -A -n \"\$cert_name\" -t \"TCu,Cu,Tu\" -i \"$cert_path\" -d dbm:\"\$certdir\""
+         done
+         # first output result fo command: find "\$HOME_DIR/.mozilla/firefox" -name "cert9.db" | while read -r certDB
+         command_call "find \$HOME_DIR/.mozilla/firefox -name \"cert9.db\""
+         find "\$HOME_DIR/.mozilla/firefox" -name "cert9.db" | while read -r certDB
+         # then use this result in cycle
+         do
+            certdir=\$(dirname "\$certDB")
+            variable_value certdir
+            command_call "certutil -A -n \"\$cert_name\" -t \"TCu,Cu,Tu\" -i \"\$cert_path\" -d sql:\"\$certdir\""
+         done
+
+         # install chrome certs
+         command_call "certutil -d sql:\$HOME_DIR/.pki/nssdb -A -t \"C,,\" -n \"TensorCA\" -i \"\$cert_path\""
+      fi
+
+      # create desktop icon
+      echo_with_log "Создание иконки на рабочем столе"
+      # first check running: source "\$HOME_DIR/.config/user-dirs.dirs"
+      command_call "source \"\$HOME_DIR/.config/user-dirs.dirs\""
+      # the immediately run: source "\$HOME_DIR/.config/user-dirs.dirs" with replacing HOME -> HOME_DIR  
+      USER_DIRS=\$(sed "s/HOME/HOME_DIR/g" "\$HOME_DIR/.config/user-dirs.dirs")
+      eval "\$USER_DIRS"
+
+      command_call "cp \"\$app_info_path\" \"\$XDG_DESKTOP_DIR\""
+
+      # mark sbis3plugin app icon as trusted
+      if [ "\$os_name" == "ubuntu" ] && command -v gio; then
+         dbus-launch gio set "\$XDG_DESKTOP_DIR/\$app_info_name" metadata::trusted "true"
+      fi
+
+      command_call "chmod +x \"\$XDG_DESKTOP_DIR/\$app_info_name\""
+   fi
+fi
+
+EOF
+
+chmod +x "$per_user_install"
+
+# Нет смысла заниматься "перенаправлением" этой команды в лог, поскольку это вызов "нас самих"
+# но с привилигией SUDO_USER, поскольку это приведет к двойному выводу в лог одного и того-же
+# выведем в лог лишь код результата запуска
+if [ "$OWNER" != "root" ]; then
+   sudo -E -u "$OWNER" bash "$per_user_install"
+   echo_log_only "Exit code for command: \"sudo -E -u \"$OWNER\" bash \"$per_user_install\"\" is $?"
+fi
+
+chown "$OWNER" -R "$sbis3plugin_path"
+
+if [ "$OWNER" != "root" ] && [ ! -z "$DESKTOP_SESSION" ]; then
+   success_flag_path="/usr/share/Sbis3Plugin/checking/21.4204.33"
+   command_call "mkdir -p \"$success_flag_path\""
+   command_call "touch \"$success_flag_path/success\""
+   
+   OWNER_TMPDIR=$(sudo -Hiu "$OWNER" env | grep TMPDIR)
+   variable_value OWNER_TMPDIR
+   
+   sudo $OWNER_TMPDIR -E -u "$OWNER" "$sbis3plugin_path/sbis3plugin" "install_done" &>/dev/null &
+   echo_with_log "Установка завершена."
+else
+   echo_with_log "Запуск плагина запрещён."
+   echo_with_log "Пожалуйста, запустите приложение самостоятельно с помощью ярлыка на рабочем столе."
+fi
+
+echo_with_log "-------------"
